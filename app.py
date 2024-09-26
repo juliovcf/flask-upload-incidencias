4bd123cfd3a733880826a4cdf9a76cdd7ea6b721
Julio Perez Cabeza <jperecab@mercadona.es>
2024-09-26 15:36:06 +0200

    bugfix export excel

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, send_file
 import sqlite3
 import pandas as pd
+from io import BytesIO
+from excel_exporter import ExcelExporter
 
 app = Flask(__name__)
 app.secret_key = "secret_key_for_flask_flash_messages"  # Necesario para usar mensajes flash en Flask
+
+# Crear una instancia de ExcelExporter
+excel_exporter = ExcelExporter()
 
 # Ruta principal para subir el archivo y mostrar la tabla
 @app.route('/', methods=['GET', 'POST'])
@@ -120,6 +125,33 @@ def upload_file_and_show_data():
 
     # Renderizar la página con la tabla, el formulario de fechas y el formulario para subir archivo
     return render_template('index.html', table=table_html, start_date=start_date, end_date=end_date)
+
+# Nueva ruta para exportar la tabla en Excel
+@app.route('/export_excel', methods=['GET'])
+def export_excel():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    filter_castles = request.args.get('filter_castles')
+
+    # Llamar al método export_to_excel de la clase ExcelExporter
+    output = excel_exporter.export_to_excel(start_date, end_date, filter_castles)
+
+    # Construir el nombre del archivo en función de los filtros
+    file_name = "incidencias"
+    
+    if start_date and end_date:
+        file_name += f"_{start_date}_to_{end_date}"
+
+    if filter_castles == 'true':
+        file_name += "_castles"
+
+    file_name += ".xlsx"  # Añadir la extensión del archivo
+
+    # Enviar el archivo como respuesta con el nombre generado
+    return send_file(output, 
+                     mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                     download_name=file_name,  # Usar el nombre dinámico
+                     as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
